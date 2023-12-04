518ef0ccd8ce5ba69c21aad0d8f4a7bb6f8d0a8d
Ho Xuan ty <hoxuanty2003qng@gmail.com>
2023-12-04 14:02:26 +0700

    test:finish text3 file

---
+++ text1.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function text1() {
+  return (
+    <View>
+      <Text>text1</Text>
+    </View>
+  )
+}
---
+++ text2.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function text2() {
+  return (
+    <View>
+      <Text>text2</Text>
+    </View>
+  )
+}
